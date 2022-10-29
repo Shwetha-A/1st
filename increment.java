1abb6ac7eede156c6a76f4c1fcbf95e3adafaba2
Shwetha-A <116061230+Shwetha-A@users.noreply.github.com>
2022-10-29 19:30:12 +0530

    Add files via upload

---
+++ increment.java
@@ -0,0 +1,16 @@
+package increment1;
+
+public class increment {
+	public static void main(String[] args) {
+
+		 int val3=10;
+		 int val4=20;
+
+		// incrementing both pre and post fix
+		System.out.println("incrementing both pre and post fix");
+		System.out.println("pre-incrementing val1 :  "+ ++val3);
+		System.out.println("pre-incrementing val2 :  "+ ++val4);
+		System.out.println("post-incrementing val1 :  "+ val3++);
+		System.out.println("post-incrementing val2 :  "+ val4++);
+	}
+}
